d4f15b07772d015fa2e541d630beb02202e844c0
Daniel Rodas <daniel.rodas1@hotmail.com>
2024-05-14 11:04:39 -0700

    Create variables.tf

---
+++ modules/examples/asev3/variables.tf
@@ -0,0 +1,16 @@
+variable "tf_token" {
+  type      = string
+  sensitive = true
+  default   = null
+}
+
+variable "organization" {
+  type = string
+  default = "rodasnet"
+}
+
+ variable "github_oauth_token_id" {
+   type = string
+   default = "ot-yUp4vcJK68UPqkUa"
+ }
+  
